7910e2d87c20aadb5e5a22bce16b908ba8fe5c68
Ryan <fullstackryan@gmail.com>
2021-09-12 15:07:30 +0100

    decoding timestamp

--- src/main/scala/fullstackryan/com/greedycryptotrader/model/CryptoData.scala
+++ src/main/scala/fullstackryan/com/greedycryptotrader/model/CryptoData.scala
@@ -4,25 +4,31 @@ import io.circe.generic.codec.DerivedAsObjectCodec.deriveCodec
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder, Json}
 
-case class CryptoData(value: String, valueClassification: Sentiment)
+import java.time.{Instant, ZoneId, ZonedDateTime}
+
+case class CryptoData(value: String, valueClassification: Sentiment, timeStamp: ZonedDateTime)
 
 object CryptoData {
 
-//  implicit val encoder: Encoder[CryptoData] = io.circe.generic.semiauto.deriveEncoder
+  // Instant.ofEpochMilli is expecting milliseconds so we times it by 1000.
+  def millisecondsToZDT(seconds: Long): ZonedDateTime =  ZonedDateTime.ofInstant(Instant.ofEpochMilli(seconds * 1000), ZoneId.systemDefault())
+
 
   implicit val cryptoDecoder: Decoder[CryptoData] = Decoder.instance { json =>
     val data = json.downField("data").downArray
     for {
-      value <- data.get[String]("value")
+      value               <- data.get[String]("value")
       valueClassification <- data.get[Sentiment]("value_classification")
-    } yield CryptoData(value, valueClassification)
+      timestamp           <- data.get[String]("timestamp")
+    } yield CryptoData(value, valueClassification, millisecondsToZDT(timestamp.toLong))
   }
 
   implicit val encoder: Encoder[CryptoData] = Encoder.instance {
-    case CryptoData(value, valueClassification) =>
+    case CryptoData(value, valueClassification, timeStamp) =>
       Json.obj(
         "value" := value.asJson,
         "value_classification" := valueClassification.asJson,
+        "timestamp" := timeStamp.asJson
       )
   }
 
